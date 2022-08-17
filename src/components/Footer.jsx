d582f2a80265e7f9d1d1ef71624977a491ccc480
yairmc <yairmc666@gmail.com>
2022-08-17 15:16:01 -0700

    actualizacion 5 Footer actualizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,8 @@ const Footer = () => {
     <div className={style.conteiner}>
       <img className={style.logo} src={logo} alt="logo" />
       <div>
-              <h1 className={style.contact}>Contact to yair_mc@hotmail.com </h1>
+              <h1 className={style.contact}>Contact to </h1>
+              <h2 className={style.contact}> yair_mc@hotmail.com </h2>
               <h2 className={style.contact}>+52-6444206948</h2>
       </div>
     </div>

69c852ff4c7e05bc0fab2dca539b4de156ffea08
yairmc <yairmc666@gmail.com>
2022-08-17 15:02:16 -0700

    actualizacion 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 
-import WhoIAm from "./pages/WhoIAm"
+import Whoiam from "./pages/WhoIAm"
 import SoftSkills from "./pages/SoftSkills"
 import Projects from "./pages/Projects"
 
@@ -22,7 +22,7 @@ function App() {
         
         <div>
             <Routes>
-              <Route path="/" element={<WhoIAm />} />
+              <Route path="/" element={<Whoiam />} />
               <Route path="/softSkills" element={<SoftSkills />} />
               <Route path="/projects" element={<Projects />} />
             </Routes>

e67ef55ba45567b43bd0faabaef5d9915d8eb3ef
yairmc <yairmc666@gmail.com>
2022-08-18 14:50:37 -0700

    actualizacion responsive realizada(aun esta por corregirse cosas

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,27 +9,26 @@ import project4 from '../img/projects/project4.png'
 
 const Projects = () => {
     return (
-        <div className={style.conteiner}>
             <div className={style.imagesContainer}>
-                <div>
+                <div className={style.imagesContainerdiv}>
                     <img src={project1} alt="Project" />
                     <a href="https://y-festivalmusicarock.netlify.app"> <h1>Rock & EDM Festival</h1> </a>
                     <a href="https://github.com/yairmc/FestivalMusica.git">Click here for my Repository of this project</a>
                     <p>This project is done in HTML with styles created in scss and JavaScript elements</p>
                 </div>
-                <div>
+                <div className={style.imagesContainerdiv}>
                     <img src={project2} alt="Project" />
                     <a href="https://control-presupuestos-y.netlify.app"><h1>Control deGastos </h1></a>
                     <a href="https://github.com/yairmc/control_presupuesto.git">Click here for my Repository of this project</a>
                     <p>This project is made in React using components with styles created in css</p>
                 </div>
-                <div>
+                <div className={style.imagesContainerdiv}>
                     <img src={project3} alt="Project" />
                     <a href="https://appveterinaria-prueba-y.netlify.app"><h1>Veterinary</h1> </a>
                     <a href="https://github.com/yairmc/citas_react.git">Click here for my Repository of this project</a>
                     <p>this project is made in React only using reagents within the same html sheet with styles created in css</p>
                 </div>
-                <div>
+                <div className={style.imagesContainerdiv}>
                     <img src={project4} alt="Project" />
                     <a href="https://cotizador-criptomonedas-y.netlify.app"><h1>Cotizador Criptomonedas</h1> </a>
                     <a href="https://github.com/yairmc/cotizadorcriptomonedas_react.git">Click here for my Repository of this project</a>
@@ -37,10 +36,6 @@ const Projects = () => {
                 </div>
 
             </div>
-
-
-
-        </div>
     )
 }
 

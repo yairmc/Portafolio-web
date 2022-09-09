c5e3ef21db2953a59ea0d549ca892b7ebc1ea705
yairmc <yairmc666@gmail.com>
2022-09-09 02:21:42 -0700

    Actualziacion en el nuevo diseno de la pagina, aun expuesto a actualizaciones

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,47 @@
 import React from 'react'
 import style from '../styles/Footer.module.css'
-import logo from '../img/YLogo.png'
+import { Link } from 'react-router-dom'
+
 
 const Footer = () => {
   return (
+    <footer>
+      <div className={style.conteiner}>
 
-    <div className={style.conteiner}>
-      <div>
-              <h1 className={style.contact}>Contact to </h1>
-              <h2 className={style.contact}> yair_mc@hotmail.com </h2>
-              <h2 className={style.contact}>+52-6444206948</h2>
+        <a href="#"> <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-facebook" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
+        </svg></a>
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-twitter" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
+        </svg></a>
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <rect x="4" y="4" width="16" height="16" rx="2" />
+          <line x1="8" y1="11" x2="8" y2="16" />
+          <line x1="8" y1="8" x2="8" y2="8.01" />
+          <line x1="12" y1="16" x2="12" y2="11" />
+          <path d="M16 16v-3a2 2 0 0 0 -4 0" />
+        </svg></a>
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-instagram" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#fff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <rect x="4" y="4" width="16" height="16" rx="4" />
+          <circle cx="12" cy="12" r="3" />
+          <line x1="16.5" y1="7.5" x2="16.5" y2="7.501" />
+        </svg></a>
       </div>
-    </div>
+
+
+      <div className={style.menu}>
+        <Link to="/"> <h1>welcome</h1></Link>
+        <Link to="/whoiam"> <h1>who i am</h1></Link>
+        <Link to="/softSkills" > <h1>my skills</h1></Link>
+        <Link to="/projects" > <h1>projects</h1> </Link>
+      </div>
+      <p>@2022 Frontend Developer | All Rights Reserved</p>
+    </footer>
+
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,6 @@
 import style from '../styles/Header.module.css'
 import { Link } from 'react-router-dom'
-import logo from '../img/YLogo.png'
-import iconoIAM from '../img/boy.png'
-import iconoSkill from '../img/skills.png'
-import iconoProjects from '../img/task.png'
-import iconoWelcome from '../img/welcome.png'
+
 const Tabs = () => {
     return (
         <div className={style.conteiner}>
@@ -15,10 +11,29 @@ const Tabs = () => {
 
             <div >
                 <div className={style.icon}>
-                    <Link to="/"><img src={iconoWelcome} alt="icono" /> </Link>
-                    <Link to="/whoiam"><img src={iconoIAM} alt="icono" /> </Link>
-                    <Link to="/softSkills" ><img src={iconoSkill} alt="icono" /> </Link>
-                    <Link to="/projects" ><img src={iconoProjects} alt="icono" /> </Link>
+                    <Link to="/"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mood-smile" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <circle cx="12" cy="12" r="9" />
+                        <line x1="9" y1="10" x2="9.01" y2="10" />
+                        <line x1="15" y1="10" x2="15.01" y2="10" />
+                        <path d="M9.5 15a3.5 3.5 0 0 0 5 0" />
+                    </svg> </Link>
+                    <Link to="/whoiam"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-man" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <circle cx="12" cy="5" r="2" />
+                        <path d="M10 22v-5l-1 -1v-4a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4l-1 1v5" />
+                    </svg> </Link>
+                    <Link to="/softSkills" ><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-html5" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z" />
+                        <path d="M15.5 8h-7l.5 4h6l-.5 3.5l-2.5 .75l-2.5 -.75l-.1 -.5" />
+                    </svg> </Link>
+                    <Link to="/projects" ><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-files" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                        <path d="M15 3v4a1 1 0 0 0 1 1h4" />
+                        <path d="M18 17h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h4l5 5v7a2 2 0 0 1 -2 2z" />
+                        <path d="M16 17v2a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h2" />
+                    </svg></Link>
                 </div>
                 <div className={style.iconName}>
                     <h1>welcome</h1>

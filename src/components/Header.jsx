c2337bbce75cb801451b3f4a740ca6e8199f745b
Yair Martinez <yairmc666@gmail.com>
2022-12-19 16:17:42 -0700

    Ready to push

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,24 +12,20 @@ const Tabs = () => {
             <div >
                 <div className={style.icon}>
                     <Link to="/"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mood-smile" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <circle cx="12" cy="12" r="9" />
                         <line x1="9" y1="10" x2="9.01" y2="10" />
                         <line x1="15" y1="10" x2="15.01" y2="10" />
                         <path d="M9.5 15a3.5 3.5 0 0 0 5 0" />
                     </svg> </Link>
                     <Link to="/whoiam"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-man" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <circle cx="12" cy="5" r="2" />
                         <path d="M10 22v-5l-1 -1v-4a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4l-1 1v5" />
                     </svg> </Link>
                     <Link to="/softSkills" ><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-html5" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <path d="M20 4l-2 14.5l-6 2l-6 -2l-2 -14.5z" />
                         <path d="M15.5 8h-7l.5 4h6l-.5 3.5l-2.5 .75l-2.5 -.75l-.1 -.5" />
                     </svg> </Link>
                     <Link to="/projects" ><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-files" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <path d="M15 3v4a1 1 0 0 0 1 1h4" />
                         <path d="M18 17h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h4l5 5v7a2 2 0 0 1 -2 2z" />
                         <path d="M16 17v2a2 2 0 0 1 -2 2h-7a2 2 0 0 1 -2 -2v-10a2 2 0 0 1 2 -2h2" />
